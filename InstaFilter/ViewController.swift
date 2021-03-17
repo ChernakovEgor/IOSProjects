40b132abd8fe4f71ffdf3eb7f388811fed43d526
Egor Chernakov <chernakov.eg@gmail.com>
2021-03-17 19:19:06 +0300

    Initial Commit

--- InstaFilter/ViewController.swift
+++ InstaFilter/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensitySlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func filterChanged(_ sender: Any) {
+    }
+    
+    @IBAction func savePressed(_ sender: Any) {
+    }
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
 }
 
